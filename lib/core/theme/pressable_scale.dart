9de2b93dc7be081b18f61e663119cd9deed0bcbc
XII777 <103256384+XII777@users.noreply.github.com>
2026-08-29 21:23:23 +0530

    Create pressable_scale.dart widget for scale animation on tap

---
+++ lib/core/theme/pressable_scale.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+/// A widget that applies a scale animation when tapped.
+/// Used to provide visual feedback for interactive elements.
+class PressableScale extends StatefulWidget {
+  const PressableScale({
+    super.key,
+    required this.onTap,
+    required this.child,
+    this.scale = 0.95,
+  });
+
+  final VoidCallback onTap;
+  final Widget child;
+  final double scale;
+
+  @override
+  State<PressableScale> createState() => _PressableScaleState();
+}
+
+class _PressableScaleState extends State<PressableScale>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: widget.scale).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+    widget.onTap();
+  }
+
+  void _onTapCancel() {
+    _controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: widget.child,
+      ),
+    );
+  }
+}
